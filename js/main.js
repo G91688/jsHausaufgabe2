20ded119148b4f128afb6866d71e589284f36655
G91688 <gultacmansimova0493@gmail.com>
2022-10-06 23:34:49 +0200

    2cidir

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+/*let ad =4*4*4*4
+let adi =4**4
+console.log(ad)
+console.log(adi)*/
+
+
+/*let ad =1
+console.log (ad++)
+console.log(ad++)*/
+
+
+for(i=19; i>=1;i=i-3){
+    console.log(i)
+}
+ 
